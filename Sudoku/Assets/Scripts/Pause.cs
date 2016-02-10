b06f47845fcd172f40d6b7c1fa9fcd5d83d0fcde
mgatmayt <mgatmayt@andrew.cmu.edu>
2016-02-10 12:19:54 -0500

    Pause scene

---
+++ Sudoku/Assets/Scripts/Pause.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    public string mainmenu;
+    public string restart;
+    public string newpuzzle;
+    public bool paused;
+    public GameObject PauseMenu;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (paused == true)
+        {
+            PauseMenu.SetActive(true);
+        }
+        else
+        {
+            PauseMenu.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+	}
+
+    public void Continue()
+    {
+
+        paused = false;
+    }
+    public void Restart()
+    {
+
+    }
+    public void NewPuzzle()
+    {
+        
+    }
+    public void MainMenu()
+    {
+       
+    }
+}
